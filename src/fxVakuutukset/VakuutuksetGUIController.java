1195a523d48865e997bcb822593443318353e938
osterava <olli.s.terava@student.jyu.fi>
2023-02-03 13:02:33 +0200

    buttonit controlleriin

--- src/fxVakuutukset/VakuutuksetGUIController.java
+++ src/fxVakuutukset/VakuutuksetGUIController.java
@@ -1,10 +1,32 @@
 package fxVakuutukset;
 
-/**
- * @author olliterava
- * @version 11.1.2023
- *
- */
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import fi.jyu.mit.fxgui.ComboBoxChooser;
+import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ModalController;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+
 public class VakuutuksetGUIController {
-      // TODO
+
+
+    @FXML private Button lisaaVakuutus;
+    @FXML private Button tallenna;
+    @FXML private Button uusiAsiakas;
+    @FXML private TextField hakuehto;
+    @FXML private ComboBoxChooser<String> cbKentat;
+
+    
+    
 }
